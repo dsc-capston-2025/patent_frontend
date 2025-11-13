4f027e08b6698cf29a48dd66f52fd1d15a9a2015
Boyeon-Shin <anna007096@gmail.com>
2025-11-13 16:08:54 +0900

    feat: PatentSearch AI 랜딩 페이지 구현

--- src/components/sections/SearchSection.tsx
+++ src/components/sections/SearchSection.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Search, Sparkles } from 'lucide-react';
+import { Search } from 'lucide-react';
 import { useState } from 'react';
 import { Button } from '@/components/common';
 
